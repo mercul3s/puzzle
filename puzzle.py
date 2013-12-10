dbea09724342764906bfed7685a24952cf909199
Mercedes Coyle <mcoyle@realgravity.com>
2013-12-10 15:14:06 -0800

    added code for letter substitution according to a Caesar Cipher.

--- puzzle.py
+++ puzzle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python
 
+from sys import argv
+
 # This is the Cards Against Humanity Holiday Bullshit puzzle.
 # I'm attempting to solve it with code.
+
+code = argv[1]
 
 frequency = {}
 alphabet  = map(chr, range(65, 91))
 frequency_alphabet = ["E", "T", "A", "O", "I", "N", "S", "R", "H", "L", "D", "C", "U", "M", "F", "P", "G", "W", "Y", "B", "V", "K", "X", "J", "Q", "Z"]
-code      = "GR QRW WKURZ DZDB DBQWKLQJ ZH VHQG BRX"
-new_code  = []
+new_code  = ""
+
 # first, we'll iterate through the string and count the
 # frequency of the letters' occurrence.
-
+# we don't actually need this since I know what the cipher is now, but I'll keep it
+# in case I need to analyze code later.
 for letter in code:
     if letter == " ":
         continue
@@ -19,18 +24,17 @@ for letter in code:
     else:
         frequency[letter] = 1
 
-for key, value in sorted(frequency.iteritems(), key=lambda (k,v): (v,k)):
-    print "%s: %s" % (key, value)
-
-print frequency_alphabet
-
-# Start substituting letters:
+# Start substituting letters according to a Caesar Cipher (offset by 3 letters)
 for letter in code:
-    if letter == "Q":
-        new_code.append("A")
-    elif letter == "Z":
-        new_code.append("E")
+    if letter == " ":
+        new_code += letter
+    elif ord(letter) > 67:
+        ordinal = ord(letter) - 3
+        new_code += chr(ordinal)
     else:
-        new_code.append(letter)
+        ordinal = ord(letter) + 23
+        # print chr(ordinal)
+        new_code += chr(ordinal)
 
-print new_code
+print "Cipher:     %s" % (code)
+print "Deciphered: %s" % (new_code)
